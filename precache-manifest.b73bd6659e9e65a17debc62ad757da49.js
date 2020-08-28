2874caaa1f99396ce02615eb2964bdc16aad1027
Samuel Smock <smock.samuel@gmail.com>
2020-08-28 16:45:07 -0500

    Updates

---
+++ precache-manifest.b73bd6659e9e65a17debc62ad757da49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b3f6fcf5f77260c7b2a6b033c2fd4f",
+    "url": "/samuelsmock/ravenous_testsite.git/index.html"
+  },
+  {
+    "revision": "bad2db4a692045023e82",
+    "url": "/samuelsmock/ravenous_testsite.git/static/css/main.b46162e9.chunk.css"
+  },
+  {
+    "revision": "40a536335e184874c60b",
+    "url": "/samuelsmock/ravenous_testsite.git/static/js/2.ff10b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samuelsmock/ravenous_testsite.git/static/js/2.ff10b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad2db4a692045023e82",
+    "url": "/samuelsmock/ravenous_testsite.git/static/js/main.609d5f39.chunk.js"
+  },
+  {
+    "revision": "1ae37537b7e3a7685634",
+    "url": "/samuelsmock/ravenous_testsite.git/static/js/runtime-main.6093f131.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/samuelsmock/ravenous_testsite.git/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/samuelsmock/ravenous_testsite.git/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
